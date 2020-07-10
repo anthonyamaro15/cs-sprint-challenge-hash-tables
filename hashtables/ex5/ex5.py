12f0f40a3e46832528bce3ce5b7584a9bc3fbecf
anthony amaro <anthonyamaro@anthonys-MacBook-Pro.local>
2020-07-10 10:40:10 -0700

    finished with all 5  problems

--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,14 +1,33 @@
 # Your code here
 
 
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
     # Your code here
+    files_found = []
 
-    return result
+    directory = {}
+
+    for file in files:
+
+        parts = file.split('/')
+
+        # parts[-1] to get last item
+        filename = parts[-1]
+        # foo, bar, baz
+
+        if filename not in directory:
+            directory[filename] = []
+
+        directory[filename].append(file)
+
+    for query in queries:
+        if query in directory:
+            files_found.extend(directory[query])
+
+    return files_found
 
 
 if __name__ == "__main__":
